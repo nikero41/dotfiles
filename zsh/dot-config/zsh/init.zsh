ceb82d98279789be6e7ff0d70adcd2b3d22eeb22
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-07 17:08:21 +0100

    Set zsh options

--- zsh/dot-config/zsh/init.zsh
+++ zsh/dot-config/zsh/init.zsh
@@ -9,11 +9,23 @@ HISTFILE=~/.zsh_history
 SAVEHIST=$HISTSIZE
 HISTDUP=erase
 
+# History options
+setopt extended_history       # Write the history file in the ":start:elapsed;command" format.
+setopt inc_append_history     # Write to the history file immediately, not when the shell exits.
+setopt share_history          # Share history between all sessions.
+setopt hist_expire_dups_first # Expire duplicate entries first when trimming history.
+setopt hist_ignore_dups       # Don't record an entry that was just recorded again.
+setopt hist_ignore_all_dups   # Delete old recorded entry if new entry is a duplicate.
+setopt hist_find_no_dups      # Do not display a line previously found.
+setopt hist_ignore_space      # Don't record an entry starting with a space.
+setopt hist_save_no_dups      # Don't write duplicate entries in the history file.
+setopt hist_reduce_blanks     # Remove superfluous blanks before recording entry.
 setopt appendhistory
-setopt sharehistory
-setopt hist_ignore_space
-setopt hist_ignore_all_dups
-setopt hist_save_no_dups
+setopt hist_verify
+
+setopt interactive_comments # Allow comments in interactive shell
+setopt auto_cd              # cd into directories by typing name
+setopt pushd_ignore_dups    # Don't push duplicates
 
 # Key bindings
 bindkey " " magic-space
