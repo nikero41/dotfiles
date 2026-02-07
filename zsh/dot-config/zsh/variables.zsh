190123ff5ea726d5d6e236a149d8ce4968203025
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-07 21:06:31 +0100

    Add catppuccin border color to fzf

--- zsh/dot-config/zsh/variables.zsh
+++ zsh/dot-config/zsh/variables.zsh
@@ -23,6 +23,7 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a \
+--color=border:#6c7086,label:#cdd6f4 \
 --multi \
 --cycle"
 
