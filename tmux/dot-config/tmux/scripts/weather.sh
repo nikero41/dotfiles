8906f930b113c40eb0c8505262facc3104eb6a4c
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-15 13:48:15 +0100

    Avoid showing separator if there are no weather data

--- tmux/dot-config/tmux/scripts/weather.sh
+++ tmux/dot-config/tmux/scripts/weather.sh
@@ -5,4 +5,8 @@ PLUGIN_DIR="$TMUX_PLUGIN_PATH/tmux-weather/scripts"
 source "$PLUGIN_DIR/helpers.sh"
 WEATHER="$("$PLUGIN_DIR"/weather.sh)"
 
+if [[ -z "$WEATHER" ]]; then
+  exit 0
+fi
+
 echo "#[bg=default,fg=#{@thm_blue}]${WEATHER} #{E:@status-separator} "
